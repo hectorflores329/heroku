2477169707cdece0ff2172a9da2eba794da91bb0
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-08-11 17:22:09 -0600

    Commit

--- app.py
+++ app.py
@@ -2,17 +2,9 @@ from flask import Flask
 import folium
 import folium.plugins as plugins
 import numpy as np
-import pandas as pd
 import requests
-import geopandas
-import branca
 import json
 import requests
-from datetime import datetime, timedelta
-from folium.plugins import FloatImage
-from folium.plugins import Draw
-from folium.plugins import MiniMap
-from folium.features import GeoJsonPopup, GeoJsonTooltip
 
 app = Flask(__name__)
 
@@ -32,15 +24,14 @@ def mapa():
         zoom_start=2,
     )
 
-    folium.GeoJson(antarctic_ice_edge, name="geojson").add_to(m)
+    '''folium.GeoJson(antarctic_ice_edge, name="geojson").add_to(m)
 
     folium.TopoJson(
         json.loads(requests.get(antarctic_ice_shelf_topo).text),
-        "objects.ICVU_2019",
         name="topojson",
-    ).add_to(m)
+    ).add_to(m)'''
 
-    folium.LayerControl().add_to(m)
+    # folium.LayerControl().add_to(m)
 
     return m._repr_html_()
 
