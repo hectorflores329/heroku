cc9613fe6dd9aee3adbed4bc91fc97dd163fe837
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-08-12 15:39:42 -0600

    Commit

--- app.py
+++ app.py
@@ -134,7 +134,7 @@ def mapa():
     else:
         ubicacion = [df["lat_comuna"][indx], df["lon_comuna"][indx]]
 
-    iframe = folium.IFrame(html=html, width=250, height=350)
+    iframe = folium.IFrame(html=html, width=250, height=365)
     _popup = folium.Popup(iframe, max_width=2650)
 
     m = folium.Map(
