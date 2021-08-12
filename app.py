f8460f1fac00b6791b9f1902ccab4369e1c5a314
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-08-11 18:26:42 -0600

    Commit

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def mapa():
         "https://raw.githubusercontent.com/hectorflores329/heroku/main"
     )
     antarctic_ice_edge = f"{url}/_ICVU_2019.json"
-    antarctic_ice_shelf_topo = f"{url}/Base_ZonaLoc_Censo_MapShaper_topo.json"
+    antarctic_ice_shelf_topo = f"{url}/Base_ZonaLoc_Censo_MapShaper(1).json"
 
 
     m = folium.Map(
