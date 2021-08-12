f503b2cea24678389069dcca407d12d2b35df27e
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-08-12 15:54:18 -0600

    Commit

--- app.py
+++ app.py
@@ -102,10 +102,10 @@ def mapa():
                 <li><b>REGIÓN:</b> """ + str(df["REGION"][indx]) + """</li>
                 <li><b>PROVINCIA:</b> """ + str(df["PROVINCIA"][indx]) + """</li>
                 <li><b>COMUNA:</b> """ + str(df["COMUNA"][indx]) + """</li>
-                <li><b>HOMBRES:</b> """ + str('{:,}'.format(df["TOTAL_HOMB"].sum())) + """</li>
-                <li><b>MUJERES:</b> """ + str(df["TOTAL_MUJE"].sum()) + """</li>
-                <li><b>TOTAL PERSONAS:</b> """ + str(df["TOTAL_PERS"].sum()) + """</li>
-                <li><b>TOTAL VIVIENDAS:</b> """ + str(df["TOTAL_VIVI"].sum()) + """</li>
+                <li><b>HOMBRES:</b> """ + str('{:,}'.format(df["TOTAL_HOMB"].sum()).replace(',','.')) + """</li>
+                <li><b>MUJERES:</b> """ + str('{:,}'.format(df["TOTAL_MUJE"].sum()).replace(',','.')) + """</li>
+                <li><b>TOTAL PERSONAS:</b> """ + str('{:,}'.format(df["TOTAL_PERS"].sum()).replace(',','.')) + """</li>
+                <li><b>TOTAL VIVIENDAS:</b> """ + str('{:,}'.format(df["TOTAL_VIVI"].sum()).replace(',','.')) + """</li>
             </ul>
         </div>
 
