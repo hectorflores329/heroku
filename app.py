a8db5a8b59abbf4637b130f9a2c9f3ecccb018d0
hectorflores329 <69374321+hectorflores329@users.noreply.github.com>
2021-08-12 00:22:11 -0600

    Commit

--- app.py
+++ app.py
@@ -35,14 +35,48 @@ def mapa():
 
     salida = {'type:':'FeatureCollection','features':output_dict}
 
+    html="""
+
+        <style>
+            *{
+                font-family: Arial, Tahoma;
+                font-size: 13px;
+            }
+            
+            li{
+                list-style:none;
+                margin-left: -40px;
+            }
+
+        </style>
+
+        <h3>Datos e información</h3>
+        <div>
+            <ul>
+                <li><b>REGIÓN:</b> """ + str(df["REGION"][indx]) + """</li>
+                <li><b>PROVINCIA:</b> """ + str(df["PROVINCIA"][indx]) + """</li>
+                <li><b>COMUNA:</b> """ + str(df["COMUNA"][indx]) + """</li>
+                <li><b>HOMBRES, 2017 - 2018:</b> """ + str(df["TOTAL_HOMB"][indx]) + """</li>
+                <li><b>MUJERES, 2017 - 2018:</b> """ + str(df["TOTAL_MUJE"][indx]) + """</li>
+                <li><b>TOTAL PERSONAS, 2017 - 2018:</b> """ + str(df["TOTAL_PERS"][indx]) + """</li>
+            </ul>
+        </div>
+    """
+
+    iframe = folium.IFrame(html=html, width=250, height=210)
+    _popup = folium.Popup(iframe, max_width=2650)
+
     m = folium.Map(
         location=[-33.411165140009885, -70.66420044462977],
         zoom_start=12,
     )
 
-    folium.GeoJson(json.dumps(salida), 
+    geojson = folium.GeoJson(json.dumps(salida), 
                     name="Geojson S.1p"
                     ).add_to(m)
+
+    popup = _popup
+    popup.add_to(geojson)
 
     folium.LayerControl().add_to(m)
 
